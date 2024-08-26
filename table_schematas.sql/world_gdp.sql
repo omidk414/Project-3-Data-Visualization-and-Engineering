fb08670d9c513380cf24042e26c7e66b960be95f
Grant <163688041+grant-i@users.noreply.github.com>
2024-08-25 19:52:34 -0700

    sql pop gdp

---
+++ table_schematas.sql/world_gdp.sql
@@ -0,0 +1,5 @@
+CREATE TABLE world_gdp (
+    country VARCHAR(255) NOT NULL,
+    year INT NOT NULL,
+    gdp FLOAT
+);
---
+++ table_schematas.sql/world_population.sql
@@ -0,0 +1,5 @@
+CREATE TABLE world_population (
+    name VARCHAR(255) NOT NULL,
+    total_population FLOAT,
+    year INT
+);
